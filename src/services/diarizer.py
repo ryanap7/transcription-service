e8d040c2968000b96824bac7e540eb56b2aa9b3f
Ryan Aprianto <ryan.aprianto77777@gmail.com>
2026-05-29 16:54:38 +0700

    Adjust token diarizer

--- src/services/diarizer.py
+++ src/services/diarizer.py
@@ -34,7 +34,7 @@ class SpeakerDiarizer:
 
             self.pipeline = Pipeline.from_pretrained(
                 "pyannote/speaker-diarization-3.1",
-                use_auth_token=self.hf_token
+                token=self.hf_token
             )
 
             if torch.cuda.is_available():
